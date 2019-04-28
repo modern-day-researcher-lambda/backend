22d2607d1d98b380ede8ba6ca41154928c3ee32f
120356aa <yurlovandrew@gmail.com>
2019-04-27 17:28:39 -0700

    deploy test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 const usersRouter = require('./api/users/usersRouter.js');
 const cardsRouter = require('./api/cards/cardsRouter.js');
